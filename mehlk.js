41558e7db8501d0d79576dcb884be4c0d418d760
Mehlika9 <mehlikab9@gmail.com>
2026-04-28 17:43:57 +0300

    Create mehlk.js

---
+++ mehlk.js
@@ -0,0 +1,11 @@
+<div class="violin-case">
+  <h2>🎻 Profesyonel Sanal Keman</h2>
+  
+  <div class="visual-keyboard">
+    <div class="row">Diyezler: <span class="highlight">W - E - T - Y - U - O - P - Ğ</span></div>
+    <hr style="border: 0; border-top: 1px solid #444;">
+    <div class="row">Natureller: <span class="highlight">A - S - D - F - G - H - J - K - L - Ş - İ</span></div>
+  </div>
+
+  <p class="status-text">Önce buraya tıkla, sonra klavyenden çalmaya başla!</p>
+</div>
